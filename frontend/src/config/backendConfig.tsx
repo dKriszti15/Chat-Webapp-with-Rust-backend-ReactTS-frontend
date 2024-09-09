801e699cffcb020f1b07ce170e6958f0ac569789
daczk <kisdacz@yahoo.com>
2024-09-09 20:31:26 +0300

    setting up the chat feature using a websocketserver(written in js) realised with socket.io. the wsserver is from the base project that inspired this one

--- frontend/src/config/backendConfig.tsx
+++ frontend/src/config/backendConfig.tsx
@@ -1,6 +1,7 @@
 const backendURL = "http://localhost:8080/api";
+const websocketserverAddress = "http://localhost:8000";
 const jwtStorageName = "jwt_token";
 
-const config = { backendURL, jwtStorageName };
+const config = { backendURL, jwtStorageName, websocketserverAddress };
 
 export default config;
---
+++ websocketserver/websocketserver.js
@@ -0,0 +1,56 @@
+import { Server } from 'socket.io';
+import express from 'express';
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+const io = new Server({
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+});
+
+const server = app.listen(PORT, () => {
+    console.log(`WebSocket Server running on port: ${PORT} ...`);
+});
+
+io.attach(server);
+
+let connectedSockets = {};
+
+io.on('connection', (socket) => {
+    console.log(`Client connected: ${socket.id}`);
+
+    socket.on('register', (username) => {
+        connectedSockets[username] = socket.id;
+        io.emit('clients-total', Object.keys(connectedSockets).length);
+        console.log(`Client registered: ${username}. Total clients: ${Object.keys(connectedSockets).length}`);
+    });
+
+    socket.on('disconnect', () => {
+        for (let username in connectedSockets) {
+            if (connectedSockets[username] === socket.id) {
+                delete connectedSockets[username];
+                break;
+            }
+        }
+        io.emit('clients-total', Object.keys(connectedSockets).length);
+        console.log(`Client disconnected: ${socket.id}. Total clients: ${Object.keys(connectedSockets).length}`);
+    });
+
+    socket.on('message', (data) => {
+        socket.broadcast.emit('chatMessage', data);
+    });
+
+    socket.on('privateMessage', (data) => {
+        const destSocket = connectedSockets[data.to];
+        if (destSocket) {
+            io.to(destSocket).emit('privateChatMessage', data);
+        } else {
+            console.log('User not connected');
+        }
+    });
+});
